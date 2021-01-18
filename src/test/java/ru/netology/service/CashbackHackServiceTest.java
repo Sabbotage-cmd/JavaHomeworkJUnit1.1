985d6d3ffe07b916b6ab760d3f54e6e516d08232
Vladimir <mednisv@yandex.ru>
2021-01-18 21:29:08 +0300

    testng 1.0

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,25 +1,28 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.junit.Test
+    @Test
     public void shouldReturn1000WithAmount0() {
         int actual = service.remain(0);
         int excepted = 1000;
+
         assertEquals(actual, excepted);
     }
-    @org.junit.Test
+
+    @Test
     public void shouldReturn100WithAmount900() {
         int actual = service.remain(500);
         int excepted = 500;
+        
         assertEquals(actual, excepted);
     }
-    @org.junit.Test
+    @Test
     public void shouldReturn0WithAmount1000() {
         int actual = service.remain(1000);
         int excepted = 0;
